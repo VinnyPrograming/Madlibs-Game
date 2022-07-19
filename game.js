401b0b8934637e2e3caa3e0d72448cc1ee62fa3f
VinnyPrograming <103543816+VinnyPrograming@users.noreply.github.com>
2022-07-19 11:44:22 -0400

    Create game.js
    Added the source code

---
+++ game.js
@@ -0,0 +1,22 @@
+//╭╮╱╱╭╮╱╱╱╱╱╱╱╱╱╱╭━━━╮╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮
+//┃╰╮╭╯┃╱╱╱╱╱╱╱╱╱╱╰╮╭╮┃╱╱╱╱╱╱╱╱┃┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╯╰╮
+//╰╮┃┃╭╋━╮╭━╮╭╮╱╭╮╱┃┃┃┣━━┳╮╭┳━━┫┃╭━━┳━━┳╮╭┳━━┳━╋╮╭╯
+//╱┃╰╯┣┫╭╮┫╭╮┫┃╱┃┃╱┃┃┃┃┃━┫╰╯┃┃━┫┃┃╭╮┃╭╮┃╰╯┃┃━┫╭╮┫┃
+//╱╰╮╭┫┃┃┃┃┃┃┃╰━╯┃╭╯╰╯┃┃━╋╮╭┫┃━┫╰┫╰╯┃╰╯┃┃┃┃┃━┫┃┃┃╰╮
+//╰╯╰┻╯╰┻╯╰┻━╮╭╯╰━━━┻━━╯╰╯╰━━┻━┻━━┫╭━┻┻┻┻━━┻╯╰┻━╯
+//╱╱╱╱╱╱╱╱╱╭━╯┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+//╱╱╱╱╱╱╱╱╱╰━━╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
+
+
+console.log("Started with no issues.")
+
+const adjOne = prompt("Enter an adjective: ")
+const verbOne = prompt ("Enter a verb: ")
+const jobName = prompt("Enter a job: ")
+const favoriteClass = prompt("Enter your favorite class: ")
+
+
+const madLib = `Coding is so ${adjOne}! It makes me want to ${verbOne} One day I want to be a ${jobName} and my favorite class at school is ${favoriteClass}.`
+
+
+console.log(madLib)
